f92d1f2bdf0ee4ce77f8054bf96a159ed10d9065
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-01-25 13:35:24 +0500

    add changes to program.cs

--- Backend/PhoneBook/PhoneBook/Program.cs
+++ Backend/PhoneBook/PhoneBook/Program.cs
@@ -3,17 +3,39 @@ using PhoneBook.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddInfrastructureServices(builder.Configuration);
+#region ConfigureServices
 
-builder.Services.AddControllers();
+var services = builder.Services;
+
+// Add services to the container.
+services.AddApplicationServices(builder.Configuration);
+services.AddInfrastructureServices(builder.Configuration);
+
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", policy =>
+    {
+        policy.WithOrigins(
+            "localhost:4200",
+            "localhost:4202"
+            );
+
+        policy.AllowAnyHeader();
+
+        policy.AllowAnyMethod();
+
+    });
+});
+#endregion
 
 var app = builder.Build();
 
+#region Configure
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -21,10 +43,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
+app.Services.GetRequiredService<ILoggerFactory>().AddFile("Logs/mylog-{Date}.txt");
+
 app.UseHttpsRedirection();
+
+app.UseCors("AllowOrigin");
 
 app.UseAuthorization();
 
 app.MapControllers();
+#endregion
 
 app.Run();
